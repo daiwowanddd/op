184a633fa36d51d7f77b8df0b3953e7a9090faa0
daiwowanddd <409388913@qq.com>
2026-02-03 17:46:24 +0800

    new

--- sh/diy-part2.sh
+++ sh/diy-part2.sh
@@ -17,9 +17,8 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/ImmortalWrt/XiaoMiR3G/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/XiaoMiR3G/g' package/base-files/files/bin/config_generate
 
 git clone -b main --single-branch --filter=blob:none https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-git clone -b main --single-branch --filter=blob:none https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
-git clone -b main --single-branch --filter=blob:none https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
+
