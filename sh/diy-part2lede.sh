da57b2690042842ed6d0c9f0a9aaa50b1b5969fa
daiwowanddd <409388913@qq.com>
2026-02-26 00:27:09 +0800

    1

--- sh/diy-part2lede.sh
+++ sh/diy-part2lede.sh
@@ -20,6 +20,4 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 #sed -i 's/ImmortalWrt/XiaoMiR3G/g' package/base-files/files/bin/config_generate
 
 rm -rf package/feeds/luci/luci-app-adguardhome
-git clone -b main --single-branch --filter=blob:none https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
-#curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
